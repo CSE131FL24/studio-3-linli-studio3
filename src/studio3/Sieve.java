f97e322bed496bc49c02e2c00952b574dd377790
apple <apple@localhost>
2024-09-18 15:50:08 -0500

    Studio3

---
+++ src/studio3/Sieve.java
@@ -0,0 +1,25 @@
+package studio3;
+
+import java.util.Scanner;
+
+public class Sieve {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		System.out.println("all prime numbers up to: ");
+		int n = in.nextInt();
+
+		int[] array = new int [n+1];
+		//	int[] x = array;
+		//Assigns numbers to index
+		for (int i = 1; i <= n; i++) {
+			array[i] = n;
+			System.out.println(i);
+
+			//for (x = 0; x < n; x++) {
+
+				//}
+			}
+		}
+	}
